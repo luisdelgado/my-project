c36a2f3d64ca6c3b6ec1dbeb44f75774ef13adf8
luisdelgado <lhds@cin.ufpe.br>
2018-06-23 10:45:03 -0300

    #2 Criando data Palpite

--- app/bolao/FimBolao.hs
+++ app/bolao/FimBolao.hs
@@ -1,6 +1,11 @@
-type Name = String
-type Timecasa = String
-type Timefora = String
-type Senha = String
-type Senhaadmin = String
-data Sala a = Name Timecasa Timefora Senha Senhaadmin
+type Nome = [Char]
+type TimeCasa = [Char]
+type TimeFora = [Char]
+type Senha = [Char]
+type SenhaAdmin = [Char]
+type Usuario = [Char]
+type Gols = Int
+
+data Sala = Sala Nome TimeCasa TimeFora Senha SenhaAdmin deriving (Show)
+
+data Palpite = Palpite Nome Senha Usuario Gols Gols deriving (Show)
