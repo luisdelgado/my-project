b5f2847702462838941814659643a3e571cb979d
Paulo Luna <jpsl93@hotmail.com>
2018-06-27 01:37:26 -0300

    Add palpite user

--- src/Handler/Palpite.hs
+++ src/Handler/Palpite.hs
@@ -1,19 +1,51 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies #-}
 module Handler.Palpite where
 
 import Import
+import Yesod.Form.Bootstrap3 (BootstrapFormLayout (..), renderBootstrap3)
 
-getPalpiteR :: Handler Value
+--import Yesod.Form.Nic (YesodNic, nicHtmlField)
+--instance YesodNic App
+
+getPalpiteR :: Handler Html
 getPalpiteR = do
     palpites <- runDB $ selectList [] [Asc PalpiteId]
-    returnJson palpites
+    (palpiteWidget, enctype) <- generateFormPost entryForm
+    defaultLayout $ do
+        setTitle "Palpites"
+        $(widgetFile "palpites")
 
-postPalpiteR :: Handler Value
+
+postPalpiteR :: Handler Html
 postPalpiteR = do
-    palpite <- (requireJsonBody :: Handler Palpite)
-
-    -- The YesodAuth instance in Foundation.hs defines the UserId to be the type used for authentication.
     maybeCurrentUserId <- maybeAuthId
-    let palpite' = palpite { palpiteUserId = maybeCurrentUserId }
+    ((res,palpiteWidget),enctype) <- runFormPost entryForm
+    case res of
+        FormSuccess palpite -> do
+           let palpite' = palpite { palpiteUserId = maybeCurrentUserId }
+           palpiteId <- runDB $ insert palpite'
+           setMessage "Palpite criado"
+           redirect PalpiteR
+        _ -> do
+            setMessage "Erro na criacao do palpite"
+            redirect PalpiteR
 
-    insertedPalpite <- runDB $ insertEntity palpite'
-    returnJson insertedPalpite
+
+createSettigs str = FieldSettings
+    { fsLabel = str, fsTooltip = Nothing, fsId = Nothing, fsName = Nothing
+    , fsAttrs =
+        [ ("class", "form-control")
+        ]
+    }
+
+entryForm :: Form Palpite
+entryForm = renderBootstrap3 BootstrapBasicForm $ Palpite
+    <$> areq   textField (createSettigs "Time da casa") Nothing
+    <*> areq   intField (createSettigs "Gols Time da casa") Nothing
+    <*> areq   textField (createSettigs "Visitante") Nothing
+    <*> areq   intField (createSettigs "Gols Visitante") Nothing
+    <*> aopt   hiddenField "userId" Nothing
